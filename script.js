1a11618ff440bd4f709a8d7a14c52d6c709fd63b
1031Rayne <135682926+1031Rayne@users.noreply.github.com>
2023-06-22 18:38:31 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  const body = document.querySelector('body');
-  body.style.backgroundAttachment = 'scroll';
-}
