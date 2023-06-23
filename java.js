d45e10042ad0732dbbaf984fe15a46e608838d67
1031Rayne <135682926+1031Rayne@users.noreply.github.com>
2023-06-23 10:45:19 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+var backgroundImageElement = document.querySelector('body::before');
+backgroundImageElement.style.position = 'static';
+backgroundImageElement.style.backgroundAttachment = 'scroll';
+backgroundImageElement.style.overflow = 'auto';
