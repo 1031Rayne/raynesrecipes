daaa12eda36ff0aadbed4099c89a7d716f4cf94c
1031Rayne <135682926+1031Rayne@users.noreply.github.com>
2023-06-23 11:08:19 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,4 +1,18 @@
-var backgroundImageElement = document.querySelector('body::before');
-backgroundImageElement.style.position = 'static';
-backgroundImageElement.style.backgroundAttachment = 'scroll';
-backgroundImageElement.style.overflow = 'auto';
+// Check if the device is a mobile device based on the viewport width
+function isMobileDevice() {
+    return window.innerWidth <= 767; // Adjust the breakpoint as needed
+  }
+  
+  // Update the background attachment based on the device type
+  function updateBackgroundAttachment() {
+    var backgroundImageElement = document.querySelector('body::before');
+    if (isMobileDevice()) {
+      backgroundImageElement.style.backgroundAttachment = 'scroll';
+    } else {
+      backgroundImageElement.style.backgroundAttachment = 'fixed';
+    }
+  }
+  
+  // Call the updateBackgroundAttachment function on page load and when the window is resized
+  window.addEventListener('DOMContentLoaded', updateBackgroundAttachment);
+  window.addEventListener('resize', updateBackgroundAttachment);
